5e2ebf00d77457f2ef99451b6d20b04ef0668066
TDRoss <tyler.qed.ross@gmail.com>
2018-03-22 15:16:06 -0700

    Add files via upload

---
+++ PairwisePotential01Cuda.jl
@@ -0,0 +1,191 @@
+using JLD
+using MAT
+using Distances
+using CuArrays
+
+
+function pairwisepotential(argvec)
+    CuArrays.allowscalar(false)
+    outname=argvec[1]::String #Name of output file
+    nf = argvec[2]::Int #Number of filaments
+    ffdim = argvec[3]::Float32 #Filament field dimension
+    flen = argvec[4]::Float32 #Filament length (sets interaction cutoff)
+    mspeed = argvec[5]::Float32 #Max Speed of attraction
+    # magp  = argvec[6]::Float32 #Magnitude of potential
+
+    #Initialize constants
+    #######
+    #set  time parameters
+    dt = 2.5f0 #time step size
+    totaltime = 2400.0f0 #total simulation time
+    iters = dt:dt:totaltime
+    dspeed = mspeed*dt
+
+    #Calculate diffusion parameters
+    avdrag = ((2.0f0*pi*8.9f0*10^(-10.0f0)*flen/(log(flen/(24.0f0))-0.2f0)) +
+     (4.0f0*pi*8.9f0*10^(-10.0f0)*flen/(log(flen/(24.0f0))-0.84f0)) )/2 #Average drag on filament
+    avdiff = sqrt(2.0f0*dt*1.0f0/avdrag) #Diffusion on filament
+
+    #Initialize filaments
+    # filpos = cu(zeros(Float32,2,nf)) #Filament positions x,y
+    # filpos = cu(rand(Float32,2,nf))*ffdim*2 -ffdim #Filament  position
+    filx = cu(rand(Float32,nf))*ffdim*2 -ffdim
+    fily = cu(rand(Float32,nf))*ffdim*2 -ffdim
+
+
+
+    #History of positions
+    timesaveresolution = 10.0f0 #time resolution of output array (seconds)
+    indxstep = Int(round(timesaveresolution/dt)) #index for when to save history
+    totalindx = Int(floor(length(iters)/indxstep))+1
+    filhist = (zeros(Float32,totalindx,2,nf)) #xp, yp
+
+    xx = ([i for i in -40000:500:40000, j in -40000:500:40000][:])
+    yy = ([j for i in -40000:500:40000, j in -40000:500:40000][:])
+    mgrid = transpose([xx yy])
+    pothist = zeros(totalindx,length(xx))
+
+    #Save first time step
+    hc = 1 #Index of saved states
+    tc = 1 #Time past between saved states
+    filhist[hc,1,:] .= collect(filx)
+    filhist[hc,2,:] .= collect(fily)
+    # dif = zeros(2)
+    magc = 1.0f0*10.0f0^4.0f0
+
+    @inbounds for t in iters   #Begin simulation
+        tc+=1
+
+        #Calculate diffusion forces on filaments
+        filfx = cu(randn(nf)).*avdiff
+        filfy = cu(randn(nf)).*avdiff
+        # filforce = zeros(2,nf)
+
+        #Identify filaments in excitation zone
+        vindx = isinzone(filx,fily,t)
+
+        #Calculate pairwise distance
+        # pwdist = cu(pairwise(Euclidean(),collect(filpos[:,indxisinzone])))
+
+        #get distances in interaction range
+        # grng = 0.<pwdist
+        # ninter = sum(grng[i,:])#number of interactions
+        # diff = cu(zeros(2,length(indxisinzone),length(indxisinzone)))
+
+        diffx = broadcast(-,filx,transpose(filx))
+        diffy = broadcast(-,fily,transpose(fily))
+        pwdist = sqrt.(diffx.^2 .+ diffy.^2)
+        inzn = vindx.*(0.<pwdist.<flen).*transpose(vindx)
+        normy = sum(inzn,2)[:,1]
+        normy += (normy.<1)
+        theta = CUDAnative.atan2.(diffy,diffx)
+        # mag = dspeed./(1.0f0.+exp.(pwdist.-flen))
+        vx = dspeed.*sum(inzn.*CUDAnative.cos.(theta),1)[1,:]./normy
+        vy = dspeed.*sum(inzn.*CUDAnative.sin.(theta),1)[1,:]./normy
+
+        filfx .+= vx
+        filfy .+= vy
+
+
+        # #Calculate pairwise attraction
+        # @inbounds for i in 1:length(indxisinzone)
+        #     ninter = sum(grng[i,:])#number of interactions
+        #     if ninter>0
+        #         diff = filpos[:,indxisinzone[grng[i,:]]] .- filpos[:,indxisinzone[i]]
+        #         theta = CUDAnative.atan2.(diff[2,:],diff[1,:])
+        #         mag = dspeed./(1.+exp.(pwdist[i,find(grng[i,:])]-flen))
+        #
+        #         vx = mean(mag.*CUDAnative.cos.(theta))
+        #         vy = mean(mag.*CUDAnative.sin.(theta))
+        #
+        #         filforce[1,indxisinzone[i]]+= vx
+        #         filforce[2,indxisinzone[i]]+= vy
+        #
+        #     end
+        # end
+
+        filx .+= filfx
+        fily .+= filfy
+
+        #reflect filament back if out of bounds
+        w = abs.(filx) .> ffdim
+        nw = .~w
+        filx .= w.*(sign.(filx).*ffdim.*2.0f0 .- filx) .+ nw.*filx
+        w .= abs.(fily) .> ffdim
+        nw .= .~w
+        fily .= w.*(sign.(fily).*ffdim.*2.0f0 .- fily) .+ nw.*fily
+
+        # @inbounds for j  in 1:nf
+        #     if abs(filpos[1,j]) > ffdim
+        #       filpos[1,j] = sign(filpos[1,j])*ffdim*2.0f0 - filpos[1,j]
+        #     end
+        #     if abs(filpos[2,j]) > ffdim
+        #       filpos[2,j] = sign(filpos[2,j])*ffdim*2.0f0 - filpos[2,j]
+        #     end
+        # end
+
+        if tc == indxstep #check if it is time to save state
+          hc+=1
+          tc=0
+          filhist[hc,1,:] .= collect(filx)
+          filhist[hc,2,:] .= collect(fily)
+          # if hc==2 || hc == round(252/timesaveresolution) || hc == round(1402/timesaveresolution)
+              ptinzn = (isinzone(xx,yy,t))
+          # end
+          potdist = pairwise(Euclidean(),filhist[hc,:,collect(vindx)],mgrid[:,ptinzn])
+          potdist[potdist.>flen] = 0
+          pothist[hc,ptinzn] = dspeed.*mean(potdist,1)
+          # potdist = filx
+          # potdist = pairwise(Euclidean(),filhist[hc,:,collect(indxisinzone)],mgrid[:,ptinzn])
+          # potdist[potdist.>30000]=0
+          # pothist[hc,ptinzn] = mean(potdist,1)
+        end
+
+
+    end
+    file = matopen(outname,"w")
+    write(file,"filhist",filhist)
+    write(file,"argvec",argvec)
+    write(file,"pothist",pothist)
+    write(file,"xx",xx)
+    write(file,"yy",yy)
+    close(file)
+
+    # save(string(outname,".jld"), "filhist", filhist, "argvec", argvec)
+end
+
+function isinzone(filx,fily,t)
+    # indx = ((-30000.0f0.<filx.<-15000.0f0) .& (-30000.0f0.<fily.<30000.0f0)) .|
+    # ((-30000.0f0.<filx.<30000.0f0) .& (15000.0f0.<fily.<30000.0f0)) .|
+    # ((15000.0f0.<filx.<30000.0f0).&(-30000.0f0.<fily.<30000.0f0)) .|
+    # ((-30000.0f0.<filx.<30000.0f0).&(-30000.0f0.<fily.<-15000.0f0))
+    # indx = ((-10000.0f0.<filx.<30000.0f0) .& (-10000.0f0.<fily.<10000.0f0)) .|
+    #     ((-30000.0f0.<filx.<-10000.0f0) .& (-30000.0f0.<fily.<30000.0f0))
+
+    if t<250
+    indx = (sqrt.((filx-25000.0f0).^2+(fily+25000.0f0).^2) .<= 10000.0f0) .|
+    (sqrt.((filx).^2+(fily-17000.0f0).^2) .<= 10000.0f0)
+    elseif t<1400
+        indx =
+            (sqrt.((filx).^2+(fily-17000.0f0).^2) .<= 10000.0f0) .|
+        ((-27000.0f0.<filx.<27000.0f0) .& (-27500.0f0.<fily.<-22500.0f0))
+    else
+        # indx = (sqrt.((filx).^2+(fily-15000.0f0).^2) .<= 10000.0f0) .|
+        indx = (-30000.0f0.<fily.<19000.0f0) .& (-2500.0f0.<filx.<2500.0f0)
+    end
+
+
+    #     if t<250
+    #     indx = (sqrt.((filx-25000.0f0).^2+(fily+25000.0f0).^2) .<= 10000.0f0) .| (sqrt.((filx+25000.0f0).^2+(fily+25000.0f0).^2) .<= 10000.0f0) .|
+    #     (sqrt.((filx).^2+(fily-17000.0f0).^2) .<= 10000.0f0)
+    # elseif t<1400
+    #         indx =    (sqrt.((filx).^2+(fily-17000.0f0).^2) .<= 10000.0f0) .|
+    #         ((-27000.0f0.<filx.<27000.0f0) .& (-27500.0f0.<fily.<-22500.0f0))
+    #     else
+    #         # indx = (sqrt.((filx).^2+(fily-15000.0f0).^2) .<= 10000.0f0) .|
+    #         indx = (-30000.0f0.<fily.<19000.0f0) .& (-2500.0f0.<filx.<2500.0f0)
+    #     end
+
+    # indx = 15000.0f0.<=sqrt.(sum(filpos.^2,1)).<= 30000.0f0
+    return indx
+end
